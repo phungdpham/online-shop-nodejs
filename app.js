b7b56824cf0140601cde37db216a1ddf8111a805
Phung Pham <phung.tech@gmail.com>
2019-05-10 03:23:48 -0400

    updated mongoose dbs

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -14,11 +16,16 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  next();
+  User.findById('5cd51480610db23eb0b2f3a4')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 })
 
 app.use('/admin', adminRoutes);
@@ -26,6 +33,26 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-})
+// mongoConnect(() => {
+//   app.listen(3000);
+// })
+
+mongoose
+  .connect('mongodb+srv://phungpham:forpp87@cluster0-gbixc.mongodb.net/shop?retryWrites=true')
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Phung',
+          email: 'phung.tech@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  }).catch(err => {
+    console.log(err);
+  })
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,10 @@
 const Product = require('../models/product');
 
+
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true,
         editing: false
     });
 };
@@ -16,38 +14,43 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl;
     const price = req.body.price;
     const description = req.body.description;
-    const product = new Product(title, price, description, imageUrl);
+    const product = new Product({
+        title: title,
+        price: price,
+        description: description,
+        imageUrl: imageUrl,
+        userId: req.user
+    });
     product
-      .save()
-      .then(result => {
-        console.log('Created Product');
-        res.redirect('/admin/products');
-      })
-      .catch(err => {
-        console.log(err);
-      })
-    ;
-    res.redirect('/');
+        .save()
+        .then(result => {
+            console.log('Created Product');
+            res.redirect('/admin/products');
+        })
+        .catch(err => {
+            console.log(err);
+        })
 };
 
-// exports.getEditProduct = (req, res, next) => {
-//     const editMode = req.query.edit;
-//     if (!editMode) {
-//       return res.redirect('/');
-//     }
-//     const prodId = req.params.productId;
-//     Product.findById(prodId, product => {
-//       if (!product) {
-//         return res.redirect('/');
-//       }
-//       res.render('admin/edit-product', {
-//         pageTitle: 'Edit Product',
-//         path: '/admin/edit-product',
-//         editing: editMode,
-//         product: product
-//       });
-//     });
-//   };
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId)
+        .then(product => {
+            if (!product) {
+                return res.redirect('/');
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            });
+        });
+};
 
 exports.postEditProduct = (req, res, next) => {
     const prodId = req.body.productId;
@@ -55,29 +58,43 @@ exports.postEditProduct = (req, res, next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
-    const updatedProduct = new Product(
-        prodId, 
-        updatedTitle, 
-        updatedImageUrl,
-        updatedDesc,
-        updatedPrice
-    );
-    updatedProduct.save();
-    res.redirect('/admin/products');
+
+    Product.findById(prodId)
+        .then(product => {
+            product.title = updatedTitle;
+            product.price = updatedPrice;
+            product.description = updatedDesc;
+            product.imageUrl = updatedImageUrl;
+            return product.save()
+        })
+        .then(result => {
+            console.log('Updated Product!');
+            res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('admin/products', {
-            prods: products,
-            pageTitle: 'Admin Products',
-            path: '/admin/products',
-        });
-    });
+    Product.find()
+        // .select('title price -_id')
+        // .populate('userId', 'name')
+        .then(products => {
+            console.log(products);
+            res.render('admin/products', {
+                prods: products,
+                pageTitle: 'Admin Products',
+                path: '/admin/products',
+            });
+        })
+        .catch(err => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.ProductId;
-    Product.deleteById(prodId);
-    res.redirect('/admin/products');
+    const prodId = req.body.productId;
+    Product.findByIdAndRemove(prodId)
+        .then(() => {
+            console.log('Destroyed Product');
+            res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
 }
