20986a8dcfc60cf4ade04889d495ce53f3209c06
Phung Pham <phung.tech@gmail.com>
2019-05-23 10:00:44 -0400

    updated

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+const multer = require('multer');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -16,6 +19,28 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions',
 });
+const csrfProtection = csrf();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images')
+  },
+  filename: (req, file, cb) => {
+    cb(null,new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+      file.mimetype === 'image/png' || 
+      file.mimetype === 'image/jpg' || 
+      file.mimetype === 'image/jpeg'
+    ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+}
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -25,7 +50,9 @@ const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({storage: fileStorage, fileFilter}).single('image'))
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(
   session({
     secret: 'my secret',
@@ -35,46 +62,55 @@ app.use(
   })
 );
 
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+})
+
+
 app.use((req, res, next) => {
   if (!req.session.user) {
     return next();
   }
   User.findById(req.session.user._id)
   .then(user => {
+    
+    if (!user) {
+      return next();
+    }
     req.user = user;
     next();
   })
-  .catch(err => console.log(err));
+  .catch(err => {
+    next(new Error(err));
+  });
 })
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
+
 app.use(errorController.get404);
 
-// mongoConnect(() => {
-//   app.listen(3000);
-// })
+app.use(((error, req, res, next) => {
+  // res.redirect('/500');
+  res.status(500).render('404', { 
+    pageTitle: 'Error!', 
+    path: '/500' ,
+    isAuthenticated: req.isLoggedIn
+  });
+}))
 
 mongoose
-  .connect(
-    MONGODB_URI
-  )
+  .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Phung',
-          email: 'phung.tech@gmail.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
-    app.listen(3000);
+     app.listen(3000);
   }).catch(err => {
     console.log(err);
   })
